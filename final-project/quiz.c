1b838c4178fc241dab6da504670eb1adb7832777
Gaby Lau <gabylau3@gmail.com>
2019-06-03 17:39:06 +0000

    test 2

--- final-project/quiz.c
+++ final-project/quiz.c
@@ -1,1 +1,25 @@
-trying this
+#include <stdio.h>
+#include <string.h>
+
+int score= 0;
+
+struct Animal {
+	char Q1[200];
+	char Q2[200];
+	char Q3[200];
+};
+
+int main()
+{
+
+	printf("Welcome to the music quiz!\n");
+
+ 	char input [200];
+	char name [200];
+
+	printf("What is your name?\n");
+	fgets (input, 200, stdin);
+	sscanf(input, "%s", &name);
+	
+	
+}
